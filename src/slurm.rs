31c4c653ae4629f118f2a72a5813d4fdf367e086
Jiajie Chen <c@jia.je>
2021-06-12 20:19:07 +0800

    Fix slurm

--- src/slurm.rs
+++ src/slurm.rs
@@ -1,28 +1,19 @@
 use crate::job::{Job, JobState};
 use chrono::Utc;
-use std::collections::HashMap;
 
 pub fn parse_slurm_stat(dsv: &str) -> Vec<Job> {
     let mut res = vec![];
-    let mut index_map: HashMap<String, usize> = HashMap::new();
     for line in dsv.split("\n") {
-        if line.starts_with("ACCOUNT|") {
-            // header
-            for (index, column) in line.split("|").enumerate() {
-                index_map.insert(column.to_string(), index);
-            }
-
-            continue;
-        } else if !line.contains("|") {
+        if !line.contains(" ") || line.starts_with("JOBID") {
             continue;
         }
 
         let mut state = JobState::Unknown;
-        let columns: Vec<&str> = line.split("|").collect();
-        let id = columns[index_map["JOBID"]].trim().to_string();
-        let name = columns[index_map["NAME"]].trim().to_string();
-        let owner = columns[index_map["USER"]].trim().to_string();
-        let raw_state = columns[index_map["STATE "]].trim().to_string();
+        let columns: Vec<&str> = line.split(" ").filter(|s| s.len() > 0).collect();
+        let id = columns[0].trim().to_string();
+        let name = columns[1].trim().to_string();
+        let owner = columns[2].trim().to_string();
+        let raw_state = columns[3].trim().to_string();
         if raw_state == "PENDING" {
             state = JobState::Queuing;
         } else if raw_state == "RUNNING" {
