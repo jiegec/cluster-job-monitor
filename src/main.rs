8e5d6b04dec6ec0f2302927ea82d758cb3b51eb1
Jiajie Chen <c@jia.je>
2023-11-11 23:33:49 +0800

    Add license header to each source file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+// Copyright (C) 2020-2023 Jiajie Chen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 use chrono::{DateTime, Utc};
+use clap::Parser;
 use cluster_job_monitor::{
     job::Job, pbs::parse_pbs_stat, slack::notify_slack, slurm::parse_slurm_stat,
 };
@@ -16,8 +31,6 @@ use std::{io::Read, process::Command};
 use timeago::Formatter;
 use tokio;
 use tokio::time::sleep;
-use clap::Parser;
-
 
 #[derive(Parser)]
 struct Args {
